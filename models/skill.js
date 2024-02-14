ee4653e68bc242fa09b9a230ac964158a49670df
charanskumar <charansenthilk@gmail.com>
2024-02-14 17:01:50 -0500

    Add controller action and index function and export controller action

--- models/skill.js
+++ models/skill.js
@@ -1,7 +1,10 @@
 const skills = [
-    {id: 123456, skill: 'JavaScript'},
-    {id: 345678, skill: 'HTML'},
-    {id: 412298, skill: 'CSS'}
+    {id: 123456, skill: 'JavaScript', learned: true},
+    {id: 345678, skill: 'HTML', learned: true},
+    {id: 412298, skill: 'CSS', learned: true},
+    {id: 678965, skill: 'Python', learned: false},
+    {id: 678896, skill: 'React', learned: false},
+    {id: 404247, skill: 'MongoDB', learned: false}
 ];
 
 module.exports = {
